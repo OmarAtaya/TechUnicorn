e0e63704b35694fc3599c6cd095845f48dc59671
OmarAtaya <omar_ataya@outlook.com>
2023-06-13 19:51:44 +0300

    Header Stage 1 Complete

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,21 +1,44 @@
 import React from 'react';
 import './Header.css';
+import Bell from '../../assets/Bell.png';
+import User from '../../assets/User.png';
+import Heart from '../../assets/Heart.png';
+import Cart from '../../assets/Cart.png';
+import Search from '../../assets/Search.png';
 
 function Header() {
   return (
     <div className='header__container'>
+
       <nav>
         <ul>
-          
+          <li>Shop</li>
+          <li>Promo</li>
+          <li>About</li>
+          <li>Blog</li>
         </ul>
       </nav>
-      <form id="form" role="search">
-        <input 
-          type="text" id="query" name="q"
-          placeholder="Search what you need"
-          aria-label="Search through site content" 
-        />
-      </form>
+
+      <div className='header__search'>
+        <form id="form" role="search">
+          <input 
+            type="text" id="query" name="q"
+            placeholder="Search what you need"
+            aria-label="Search through site content" 
+          />
+          <img src={Search} alt='' width={"24px"} height={"24px"}/>
+        </form>
+      </div>
+
+      <div className='header__menu'>
+        <img src={Heart} alt='' width={"32px"} height={"32px"}/>
+        <div>
+          <img src={Cart} alt='' width={"32px"} height={"32px"}/>
+        </div>
+        <img src={User} alt='' width={"32px"} height={"32px"}/>
+        <img src={Bell} alt='' width={"32px"} height={"32px"}/>
+      </div>
+
     </div>
   )
 }
